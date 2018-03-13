335b550f397ada15cb0a979b91d6629a3f01fe3b
dannyelcf <dannyelcf@gmail.com>
2018-03-13 10:17:48 -0300

    Change the way that libraries are loaded

--- core-course-05-exploratory-data-analysis/project-05-explore-and-summarize-data/project_05.R
+++ core-course-05-exploratory-data-analysis/project-05-explore-and-summarize-data/project_05.R
@@ -1,3 +1,14 @@
+load_packages <- function(packages) {
+  sapply(packages,
+         function(pkg) {
+           tryCatch(library(pkg, character.only = TRUE, warn.conflicts = FALSE),
+                    error = function(err) {
+                      utils::install.packages(pkg, quiet = TRUE)
+                      library(pkg, character.only = TRUE, warn.conflicts = FALSE)
+                    })
+         })
+}
+
 load_dataset <- function(path) {
   # Load the Issues Tracking Data Set
   setClass('date_ymd_hms')
@@ -7,7 +18,7 @@ load_dataset <- function(path) {
   # setClass('time_period')
   # setAs("character", "time_period", function(from) seconds_to_period(from))
   setClass('factor_issue_priority_scale')
-  setAs("character", "factor_issue_priority_scale", 
+  setAs("character", "factor_issue_priority_scale",
         function(from) {
           factor(from, levels = c("SUSPENDED",
                                   "LOW",
@@ -16,8 +27,8 @@ load_dataset <- function(path) {
                                   "URGENT",
                                   "BLOCKING"))
         })
-  issues <- read.csv(path, 
-                     stringsAsFactors=FALSE, 
+  issues <- read.csv(path,
+                     stringsAsFactors=FALSE,
                      na.strings = "",
                      colClasses = c("integer", # issue_id
                                     "integer", # issue_number
@@ -43,8 +54,8 @@ load_dataset <- function(path) {
                                     "integer", # log_progress
                                     # "time_period", # log_time_spent
                                     "integer", # log_time_spent
-                                    "character", # log_created_by 
-                                    "integer" # log_svn_revision 
+                                    "character", # log_created_by
+                                    "integer" # log_svn_revision
                      ))
 }
 
@@ -54,17 +65,17 @@ df_summary <- function(df, variable) {
 
 .df_summary <- function(df, variable) {
   variable <- eval(variable, df)
-  
-  data.frame(min = min(variable), 
+
+  data.frame(min = min(variable),
              qu1 = quantile(variable, probs = .25, names = FALSE),
              median = quantile(variable, probs = .5, names = FALSE),
              mean = mean(variable),
              qu3 = quantile(variable, probs = .75, names = FALSE),
-             iqr = (quantile(variable, probs = .75, names = FALSE) - 
+             iqr = (quantile(variable, probs = .75, names = FALSE) -
                       quantile(variable, probs = .25, names = FALSE)),
              pc90 = quantile(variable, probs = .90, names = FALSE),
              max = max(variable))
-  
+
 }
 
 text_df_summary <- function(df_summary) {
@@ -78,8 +89,8 @@ text_df_summary <- function(df_summary) {
 }
 
 subtitle <- function(observations, complement = NULL, df_summary = NULL) {
-  paste0(observations, 
-         " observations ", 
+  paste0(observations,
+         " observations ",
          complement,
          if(!is.null(df_summary)) {
            paste0("\n", text_df_summary(df_summary))
@@ -91,7 +102,7 @@ subtitle <- function(observations, complement = NULL, df_summary = NULL) {
 plot_x_summary <- function(data, x) {
   x_q <- substitute(x)
   df_summary <- .df_summary(data, x_q)
-  
+
   list(
     # Summaries....
     # 1st quantile
@@ -102,14 +113,14 @@ plot_x_summary <- function(data, x) {
     geom_vline(xintercept = df_summary$qu3, linetype = 2, color = "black"),
     # Lower outlier limiar (1st qu. - IQR * 1.5)
     if(df_summary$qu1 - (df_summary$iqr * 1.5) > df_summary$min) {
-      geom_vline(xintercept = df_summary$qu1 - (df_summary$iqr * 1.5), 
+      geom_vline(xintercept = df_summary$qu1 - (df_summary$iqr * 1.5),
                  linetype = 3, color = "black")
     } else {
       geom_blank()
     },
     # Upper outlier limiar (3st qu. + IQR * 1.5)
     if(df_summary$qu3 + (df_summary$iqr * 1.5) < df_summary$max) {
-      geom_vline(xintercept = df_summary$qu3 + (df_summary$iqr * 1.5), 
+      geom_vline(xintercept = df_summary$qu3 + (df_summary$iqr * 1.5),
                  linetype = 3, color = "black")
     } else {
       geom_blank()
@@ -121,11 +132,11 @@ plot_x_summary <- function(data, x) {
 
 plot_y_summary <- function(data, y) {
   y_q <- substitute(y)
-  df_summary <- data %>% 
-                  group_by_(y_q) %>% 
-                  summarise(frequency = n()) %>% 
+  df_summary <- data %>%
+                  group_by_(y_q) %>%
+                  summarise(frequency = n()) %>%
                   df_summary(frequency)
-  
+
   list(
     # Summaries....
     # 1st quantile
@@ -136,14 +147,14 @@ plot_y_summary <- function(data, y) {
     geom_hline(yintercept = df_summary$qu3, linetype = 2, color = "black"),
     # Lower outlier limiar (1st qu. - IQR * 1.5)
     if(df_summary$qu1 - (df_summary$iqr * 1.5) > df_summary$min) {
-      geom_hline(yintercept = df_summary$qu1 - (df_summary$iqr * 1.5), 
+      geom_hline(yintercept = df_summary$qu1 - (df_summary$iqr * 1.5),
                  linetype = 3, color = "black")
     } else {
       geom_blank()
     },
     # Upper outlier limiar (3st qu. + IQR * 1.5)
     if(df_summary$qu3 + (df_summary$iqr * 1.5) < df_summary$max) {
-      geom_hline(yintercept = df_summary$qu3 + (df_summary$iqr * 1.5), 
+      geom_hline(yintercept = df_summary$qu3 + (df_summary$iqr * 1.5),
                  linetype = 3, color = "black")
     } else {
       geom_blank()
@@ -156,21 +167,21 @@ plot_y_summary <- function(data, y) {
 plot_cumsummary <- function(data, x, y) {
   x_q <- substitute(x)
   x <- eval(x_q, data)
-  
+
   if(is.null(x)) {
     stop("'x' argument can't be NULL")
   }
-  
+
   y_q <- substitute(y)
   y <- eval(y_q, data)
-  
+
   if(is.null(y)) {
     stop("'y' argument can't be NULL")
   }
-  
+
   df_cumsummary <- data %>%
                     # Create cumulative summaries variables
-                     mutate(cummean = sapply(seq_along(y), 
+                     mutate(cummean = sapply(seq_along(y),
                                              function(n) {
                                                mean(y[1:n])
                                              }),
@@ -178,36 +189,36 @@ plot_cumsummary <- function(data, x, y) {
                                                function(n) {
                                                  median(y[1:n])
                                                }),
-                            cum1stqu = sapply(seq_along(y), 
+                            cum1stqu = sapply(seq_along(y),
                                               function(n) {
                                                 quantile(y[1:n], probs = .25)
                                               }),
-                            cum3rdqu = sapply(seq_along(y), 
+                            cum3rdqu = sapply(seq_along(y),
                                               function(n) {
                                                 quantile(score[1:n], probs = .75)
                                               }))
   df_summary <- .df_summary(data, y_q)
   df_summary$max.x <- max(x)
-  
+
   list <- list(
     # Summary lines
     geom_line(data = df_cumsummary,
-              mapping = aes_string(x = deparse(x_q), y = "cummedian"), 
+              mapping = aes_string(x = deparse(x_q), y = "cummedian"),
               linetype = 1, color = "black"),
     geom_line(data = df_cumsummary,
-              mapping = aes_string(x = deparse(x_q), y = "cum1stqu"), 
+              mapping = aes_string(x = deparse(x_q), y = "cum1stqu"),
               linetype = 2, color = "black"),
     geom_line(data = df_cumsummary,
-              mapping = aes_string(x = deparse(x_q), y = "cum3rdqu"), 
+              mapping = aes_string(x = deparse(x_q), y = "cum3rdqu"),
               linetype = 2, color = "black"),
     geom_line(data = df_cumsummary,
-              mapping = aes_string(x = deparse(x_q), y = "cummean"), 
+              mapping = aes_string(x = deparse(x_q), y = "cummean"),
               linetype = 1, color = "red")
   )
 }
 
 add_label <- function(y) {
-  geom_text(aes_string(label = deparse(y)), 
+  geom_text(aes_string(label = deparse(y)),
             vjust = -1, size = 3)
 }
 
